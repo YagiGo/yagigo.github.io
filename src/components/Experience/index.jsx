7e1d9899e4f44848706f8c0bef8bd70090517e4d
yagigo <jeremywu1995@gmail.com>
2022-01-27 21:40:14 +0900

    update experience page - EN ver

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -9,78 +9,78 @@ class Experience extends Component {
                     <h2 className="mb-5">Education & Work Experience</h2>
                     <div className="resume-item d-flex flex-column flex-md-row">
                         <div className="resume-content mr-auto">
-                            <h4 className="mb-0">中国・北京交通大学</h4>
-                            <div className="subheading mb-3"><small>通信工学専攻</small></div>
+                            <h4 className="mb-0">{t('experience.college.name')}</h4>
+                            <div className="subheading mb-3"><small>{t('experience.college.major')}</small></div>
                         </div>
                         <div className="resume-date text-md-right">
-                            <span className="text-primary">2013年9月 - 2017年7月</span>
+                            <span className="text-primary">{t('experience.college.duration')}</span>
                         </div>
                     </div>
 
                     <div className="resume-item d-flex flex-column flex-md-row">
                         <div className="resume-content mr-auto">
-                            <h4 className="mb-0">日本・筑波大学</h4>
-                            <div className="subheading mb-0"><small>システム情報工学研究科</small></div>
-                            <div className="subheading mb-3"><small>コンピュターサイエンス専攻</small></div>
+                            <h4 className="mb-0">{t('experience.graduate_school.name')}</h4>
+                            <div className="subheading mb-3"><small>{t('experience.graduate_school.major')}</small></div>
                         </div>
                         <div className="resume-date text-md-right">
-                            <span className="text-primary">2018年4月 - 2020年4月 </span>
+                            <span className="text-primary">{t('experience.graduate_school.duration')}</span>
                         </div>
                     </div>
 
                     <div className="resume-item d-flex flex-column flex-md-row">
                         <div className="resume-content mr-auto">
-                            <h4 className="mb-0">日本・東京</h4>
-                            <div className="subheading mb-0"><small>ソフトバンク株式会社</small></div>
-                            <div className="subheading mb-1"><small>インターンシップ</small></div>
-                            <p>主にiOS/Androidアプリの開発を行いました</p>
+                            <h4 className="mb-0">{t('experience.internship.location')}</h4>
+                            <div className="subheading mb-0"><small>{t('experience.internship.name')}</small></div>
+                            <div className="subheading mb-1"><small>{t('experience.internship.type')}</small></div>
+                            <p>{t('experience.internship.content')}</p>
                         </div>
                         <div className="resume-date text-md-right">
-                            <span className="text-primary">2018年8月20日 - 2018年9月14日 </span>
+                            <span className="text-primary">{t('experience.internship.duration')}</span>
                         </div>
                     </div>
 
                     <div className="resume-item d-flex flex-column flex-md-row">
                         <div className="resume-content mr-auto">
-                            <h4 className="mb-0">日本・東京</h4>
-                            <div className="subheading mb-0"><small> SBイノベンチャー株式会社</small></div>
-                            <div className="subheading mb-1"><small>エンジニア 長期アルバート</small></div>
-                            <p> 主にAWS/PHP/jQuery/Bootstrap/mySQLで社内システムの開発を行いました</p>
+                            <h4 className="mb-0">{t('experience.part_time.location')}</h4>
+                            <div className="subheading mb-0"><small> {t('experience.part_time.name')}</small></div>
+                            <div className="subheading mb-1"><small>{t('experience.part_time.type')}</small></div>
+                            <p>{t('experience.part_time.content')}</p>
                         </div>
                         <div className="resume-date text-md-right">
-                            <span className="text-primary">2019年5月 - 2020年3月</span>
+                            <span className="text-primary">{t('experience.part_time.duration')}</span>
                         </div>
                     </div>
 
                     <div className="resume-item d-flex flex-column flex-md-row">
                         <div className="resume-content mr-auto">
-                            <h4 className="mb-0">日本・東京</h4>
-                            <div className="subheading mb-0"><small> ソフトバンク株式会社</small></div>
-                            <div className="subheading mb-3"><small>フルスタックエンジニア</small></div>
+                            <h4 className="mb-0">{t('experience.full_time.location')}</h4>
+                            <div className="subheading mb-0"><small> {t('experience.full_time.name')}</small></div>
+                            <div className="subheading mb-1"><small>{t('experience.full_time.type')}</small></div>
+                            <p>{t('experience.full_time.content')}</p>
                         </div>
                         <div className="resume-date text-md-right">
-                            <span className="text-primary">2020年4月</span>
+                            <span className="text-primary">{t('experience.part_time.duration')}</span>
                         </div>
                     </div>
 
                     <div className="resume-item d-flex flex-column flex-md-row mb-5">
                         <div className="resume-content mr-auto">
-                            <h4 className="mb-0">日本語能力試験N1合格</h4>
-                            <p>点数：142/180（ビジレスレベル）</p>
+                            <h4 className="mb-0">{t('experience.english.level')}</h4>
+                            <p>{t('experience.english.note')}</p>
                         </div>
                     </div>
 
                     <div className="resume-item d-flex flex-column flex-md-row mb-5">
                         <div className="resume-content mr-auto">
-                            <h4 className="mb-0">TOEIC試験</h4>
-                            <p>点数：980/990（ビジレスレバル）</p>
+                            <h4 className="mb-0">{t('experience.japanese.level')}</h4>
+                            <p>{t('experience.japanese.note')}</p>
                         </div>
                     </div>
 
                     <div className="resume-item d-flex flex-column flex-md-row mb-5">
                         <div className="resume-content mr-auto">
-                            <h4 className="mb-0">中国語</h4>
-                            <p>Native Speaker</p>
+                            <h4 className="mb-0">{t('experience.chinese.level')}</h4>
+                            <p>{t('experience.chinese.note')}</p>
                         </div>
                     </div>
 
