24d607afbef5c3a75bb964afd2e5e4173a6186bd
yagigo <jeremywu1995@gmail.com>
2022-01-28 12:29:39 +0900

    update work page - EN ver

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -9,60 +9,55 @@ class Experience extends Component {
                     <h2 className="mb-5">Education & Work Experience</h2>
                     <div className="resume-item d-flex flex-column flex-md-row">
                         <div className="resume-content mr-auto">
-                            <h4 className="mb-0">{t('experience.college.name')}</h4>
-                            <div className="subheading mb-3"><small>{t('experience.college.major')}</small></div>
+                            <h4 className="mb-0">{t('experience.full_time.name')}</h4>
+                            <div className="subheading mb-0"><small> {t('experience.full_time.location')}</small></div>
+                            <div className="subheading mb-1"><small>{t('experience.full_time.type')}</small></div>
+                            <p>{t('experience.full_time.content')}</p>
                         </div>
-                        <div className="resume-date text-md-right">
-                            <span className="text-primary">{t('experience.college.duration')}</span>
+                        <div className="resume-date text-md-left">
+                            <span className="text-primary">{t('experience.full_time.duration')}</span>
                         </div>
                     </div>
-
+                    <div className="resume-item d-flex flex-column flex-md-row">
+                        <div className="resume-content mr-auto">
+                            <h4 className="mb-0">{t('experience.part_time.name')}</h4>
+                            <div className="subheading mb-0"><small> {t('experience.part_time.location')}</small></div>
+                            <div className="subheading mb-1"><small>{t('experience.part_time.type')}</small></div>
+                            <p>{t('experience.part_time.content')}</p>
+                        </div>
+                        <div className="resume-date text-md-left">
+                            <span className="text-primary">{t('experience.part_time.duration')}</span>
+                        </div>
+                    </div>
+                    <div className="resume-item d-flex flex-column flex-md-row">
+                        <div className="resume-content mr-auto">
+                            <h4 className="mb-0">{t('experience.internship.name')}</h4>
+                            <div className="subheading mb-0"><small>{t('experience.internship.location')}</small></div>
+                            <div className="subheading mb-1"><small>{t('experience.internship.type')}</small></div>
+                            <p>{t('experience.internship.content')}</p>
+                        </div>
+                        <div className="resume-date text-md-left">
+                            <span className="text-primary">{t('experience.internship.duration')}</span>
+                        </div>
+                    </div>
                     <div className="resume-item d-flex flex-column flex-md-row">
                         <div className="resume-content mr-auto">
                             <h4 className="mb-0">{t('experience.graduate_school.name')}</h4>
                             <div className="subheading mb-3"><small>{t('experience.graduate_school.major')}</small></div>
                         </div>
-                        <div className="resume-date text-md-right">
+                        <div className="resume-date text-md-left">
                             <span className="text-primary">{t('experience.graduate_school.duration')}</span>
                         </div>
                     </div>
-
                     <div className="resume-item d-flex flex-column flex-md-row">
                         <div className="resume-content mr-auto">
-                            <h4 className="mb-0">{t('experience.internship.location')}</h4>
-                            <div className="subheading mb-0"><small>{t('experience.internship.name')}</small></div>
-                            <div className="subheading mb-1"><small>{t('experience.internship.type')}</small></div>
-                            <p>{t('experience.internship.content')}</p>
+                            <h4 className="mb-0">{t('experience.college.name')}</h4>
+                            <div className="subheading mb-3"><small>{t('experience.college.major')}</small></div>
                         </div>
-                        <div className="resume-date text-md-right">
-                            <span className="text-primary">{t('experience.internship.duration')}</span>
+                        <div className="resume-date text-md-left">
+                            <span className="text-primary">{t('experience.college.duration')}</span>
                         </div>
                     </div>
-
-                    <div className="resume-item d-flex flex-column flex-md-row">
-                        <div className="resume-content mr-auto">
-                            <h4 className="mb-0">{t('experience.part_time.location')}</h4>
-                            <div className="subheading mb-0"><small> {t('experience.part_time.name')}</small></div>
-                            <div className="subheading mb-1"><small>{t('experience.part_time.type')}</small></div>
-                            <p>{t('experience.part_time.content')}</p>
-                        </div>
-                        <div className="resume-date text-md-right">
-                            <span className="text-primary">{t('experience.part_time.duration')}</span>
-                        </div>
-                    </div>
-
-                    <div className="resume-item d-flex flex-column flex-md-row">
-                        <div className="resume-content mr-auto">
-                            <h4 className="mb-0">{t('experience.full_time.location')}</h4>
-                            <div className="subheading mb-0"><small> {t('experience.full_time.name')}</small></div>
-                            <div className="subheading mb-1"><small>{t('experience.full_time.type')}</small></div>
-                            <p>{t('experience.full_time.content')}</p>
-                        </div>
-                        <div className="resume-date text-md-right">
-                            <span className="text-primary">{t('experience.part_time.duration')}</span>
-                        </div>
-                    </div>
-
                     {/*<div className="resume-item d-flex flex-column flex-md-row mb-5">*/}
                     {/*    <div className="resume-content mr-auto">*/}
                     {/*        <h4 className="mb-0">{t('experience.english.level')}</h4>*/}
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -6,75 +6,45 @@ class Projects extends Component {
         return (
             <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
                 <div className="my-auto">
-                    <h2 className="mb-0">Project</h2>
-                    <div className="subheading mb-5"><h3>現在の業務内容</h3></div>
+                    <h2 className="mb-0">Work</h2>
                     <div className="resume-item d-flex flex-column flex-md-row mb-3">
                         <div className="resume-content mr-auto">
-                            <h3 className="mb-0 pb-1">Axross-AI教育サービス</h3>
-                            <p><a href="https://axross-recipe.com" target="_blank" rel="noreferrer">Axross</a>とは「学んだが活用できない人」を減らしたい想いのもと、現役エンジニアのナレッジを学習教材 "レシピ" として提供するプラットフォームです。
-                                YOLOやPyTorch, BERT, MoveNet, TensorFlowなどモダンな技術を気軽に触れます。
+                            <h3 className="mb-0">Softbank Corp.</h3>
+                            <div className='subheading'>Axross-AI (ongoing)</div>
+                            <div className='subheading mb-3'>Front-End, Back-End</div>
+                            <p><a href="https://axross-recipe.com" target="_blank" rel="noreferrer">Axross</a> is a online education service which provides AI related learning materials for developers who are not very familiar with AI field.
                             </p>
-                            <p>[使用している言語・フレームワーク]TypeScript/NextJS/Webpack/ESLint, Ruby/Ruby on Rails/MySQL/redis, GCP/Githubなど</p>
+                            <ul>
+                                <li>Building stable and maintainable front-end codebases using TypeScript, React and nextJS.</li>
+                                <li>Building corresponding RESTful APIs and related back-end services using Ruby and Ruby on Rails.</li>
+                                <li>Designing and implementing UI from scratch while working closely with other developers to make sure consistent API and interface integration. </li>
+                                <li>Creating mobile-first responsive templates with modern CSS techniques and JavaScript libraries.</li>
+                                <li>Conducting unit tests and E2E tests with automated workflow.</li>
+                                <li>Maintaining current infrastructure with 3-rd party libraries. As well as performing bugs fixes and helping out other developers along the process. </li>
+                                <li>Ensuring proper documentation and reports in all stages of product life cycle. Managing and tracking progresses with tools such as Jira and agile methodology.</li>
+                            </ul>
+                            <p>[Tech Stack] TypeScript/NextJS/Webpack/ESLint, Ruby/Ruby on Rails/MySQL/redis, GCP/Github etc.</p>
                         </div>
                     </div>
-                    <div className="subheading mb-5 mt-5"><h3>今まで行なったプログラミングプロジェクトについて紹介します</h3></div>
-
-                    <div className="resume-item d-flex flex-column flex-md-row mb-5">
+                    <div className="resume-item d-flex flex-column flex-md-row mb-3">
                         <div className="resume-content mr-auto">
-                            <h4 className="mb-0">エッジコンピューティングによるモバイルデバイス向けのウェブブラウザ</h4>
-                            <div className="subheading mb-3">大学院の研究内容</div>
-                            <p>エッジサーバーは代理人として稼働し、クラアント側の負担を減らすことを目指す。この目標を実現するために二つの手法を提案する。
-                                一つの手法はエッジキャッシュと呼ばれる。エッジサーバーは一つのウェブページを丸ごとにキャッシュすることが可能になる。ユーザーはあるキャッシュされたウェブサイトをアクセスするときに、
-                                エッジサーバーはそのサイトのファイルをまとめ、ユーザーに送る。それに、エッジキャッシュは学習で自動的にキャッシュを更新することができる。従来のウェブページダウンロードと比べると、接続数を減らし、
-                                ウェブページのローディング時間を短縮することができる。もう一つの手法はエッジレンダリングである。ウェブページのローディングの中に重いタスクをエッジサーバーにオフロードすることにより、更にウェブページダウンロード時間を短縮することが可能になる。
+                            <h3 className="mb-0">Softbank Innoventure Corp.</h3>
+                            <div className='subheading'>Human Resource Tool (2019-2020)</div>
+                            <div className='subheading mb-3'>Front-End, Back-End</div>
+                            <p> Helped create a internal HR tool to help employees who seek to start a business find partners. It also provides SNS features and is seamlessly integrated with both internal and external platforms such as HubSpot.
                             </p>
-                            <p>[研究キーワード]Edge Computing、ウェブブラウザ、キャッシュ 、ウェブレンダリング</p>
-                            <p>[実装に使用した言語・フレームワーク]Python, JavaScript, NodeJS, PhantomJS</p>
-                            <p>[国際会議での発表論文]<strong>Improving Web Browsing Experience with Personalized Edge Computing.</strong> 2019 IEEE International Conferences on Ubiquitous Computing & Communications (IUCC) and Data Science and Computational Intelligence (DSCI) and Smart Computing, Networking and Services (SmartCNS). IEEE, 2019.</p>
+                            <ul>
+                                <li>Building stable and maintainable front-end codebases using jQuery and bootstrap.</li>
+                                <li>Building related back-end services with PHP.</li>
+                                <li>Designing and implementing UI from scratch while working closely with other developers to make sure consistent API and interface integration. </li>
+                                <li>Integrating 3rd-party platforms such as SendGrid and HubSpot.</li>
+                                <li>Maintaining current infrastructure with 3-rd party libraries. As well as performing bugs fixes and helping out other developers along the process. </li>
+                                <li>Ensuring proper documentation and reports in all stages of product life cycle. Managing and tracking progresses with tools such as Jira and agile methodology.</li>
+                            </ul>
+                            <p>[Tech Stack] JavaScript/jQuery/Bootstrap/PHP/AWS etc.</p>
                         </div>
                     </div>
-
-                    <div className="resume-item d-flex flex-column flex-md-row mb-5">
-                        <div className="resume-content mr-auto">
-                            <h4 className="mb-0">ディープラーニングの方法でオンラインレビュー向けの感情分析システム</h4>
-                            <div className="subheading mb-3">大学の卒論研究内容</div>
-                            <p>オンラインショッピングサイトでの商品レビューをPythonで開発した分散ウェブクローラを利用してゲットしてそのレビューデータを利用し、自然言語処理方法（主にディープラーニング）による分析し、感情分析用のモデルを作り、そのモデルで感情分析システムを作成した。このシステムを利用すれば、オンラインレビュー（ニュースコメント、商品やレストランのレビューなど）の感情分析ができる。</p>
-                            <p>[研究キーワード]自然言語処理、データクローリング、ディープラーニング</p>
-                            <p>[実装に使用した言語・フレームワーク]Python, Tensorflow</p>
-                            <div className="os-ref mr-auto">
-                                <a href="https://github.com/YagiGo/FeedbackSentimentClassfication">https://github.com/YagiGo/FeedbackSentimentClassfication</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="resume-item d-flex flex-column flex-md-row mb-5">
-                        <div className="resume-content mr-auto">
-                            <h4 className="mb-0">YPTN Profiler</h4>
-                            <div className="subheading mb-3">個人プロジェクト</div>
-                            <p>YPTN
-                                Profilerとはウェブページパフォーマンスの分析ツールです。ウェブページのURLを入力するだけで、ウェブページの接続の請求や請求されるファイルの種類とサイズなどの情報が滝グラフで直観的に表示できる。Flaskによるサーバーで簡単にデプロイできる。<br/>GoogleやFirefoxのDeveloper
-                                    Toolsと似てるところもありますが、
-                                    APIを提供するので、他のプログラムで簡単に使えます。
-                            </p>
-                            <p>[実装に使用した言語・フレームワーク]Python, JavaScript</p>
-                            <div className="os-ref mr-auto">
-                                <a href="https://github.com/YagiGo/YPTN_Profiling_Server">https://github.com/YagiGo/YPTN_Profiling_Server</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="resume-item d-flex flex-column flex-md-row">
-                        <div className="resume-content mr-auto">
-                            <h4 className="mb-0">個人ブログサイト</h4>
-                            <div className="subheading mb-3">個人プロジェクト</div>
-                            <p>バックエンド Flask+Nginx フロントエンド jQuery, Bootstrap データベースSQLite
-                                Markdown機能を支援するブログです。</p>
-                            <p>[実装に使用した言語・フレームワーク]Python, JavaScript, jQuery, Bootstrap, SQLite</p>
-                            <div className="os-ref mr-auto">
-                                <a href="https://www.zhaoxinblog.com">https://www.zhaoxinblog.com</a>
-                            </div>
-                        </div>
-                    </div>
+                    <h2 className="mb-0">Pet Projects & Previous Research</h2>
                 </div>
             </section>
         );
